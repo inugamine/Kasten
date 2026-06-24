9206ad71294ab20188aecf3aebe822e0cc7dd57a
inugamine <zh20d0m0iztxkg5f5914@gmail.com>
2026-06-24 21:05:33 +0900

    SwiftTerm-fork に向くように修正

--- Kasten/KastenApp.swift
+++ Kasten/KastenApp.swift
@@ -7,6 +7,7 @@
   
 
 import SwiftUI
+import AppKit
 
 @main
 struct KastenApp: App {
@@ -16,5 +17,62 @@ struct KastenApp: App {
                 .frame(minWidth: 700, minHeight: 450)
         }
         .windowResizability(.contentMinSize)
+        .commands {
+            // 標準の「Kasten について」を、利用 OSS のライセンス表示付きに差し替える。
+            CommandGroup(replacing: .appInfo) {
+                Button("Kasten について") {
+                    KastenApp.showAboutPanel()
+                }
+            }
+        }
+    }
+
+    /// About パネルを開き、利用しているオープンソース（SwiftTerm / MIT ライセンス）の
+    /// 著作権表示とライセンス本文を credits 欄に表示する。MIT の帰属表示義務を満たすため。
+    @MainActor
+    static func showAboutPanel() {
+        let license = """
+        SwiftTerm
+
+        Copyright (c) 2019-2022 Miguel de Icaza (https://github.com/migueldeicaza)
+        Copyright (c) 2017-2019, The xterm.js authors (https://github.com/xtermjs/xterm.js)
+        Copyright (c) 2014-2016, SourceLair Private Company (https://www.sourcelair.com)
+        Copyright (c) 2012-2013, Christopher Jeffrey (https://github.com/chjj/)
+
+        Permission is hereby granted, free of charge, to any person obtaining
+        a copy of this software and associated documentation files (the
+        "Software"), to deal in the Software without restriction, including
+        without limitation the rights to use, copy, modify, merge, publish,
+        distribute, sublicense, and/or sell copies of the Software, and to
+        permit persons to whom the Software is furnished to do so, subject to
+        the following conditions:
+
+        The above copyright notice and this permission notice shall be
+        included in all copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+        NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+        LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+        OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+        WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        """
+
+        let header = "Kasten は以下のオープンソースソフトウェアを利用しています。\n\n"
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 2
+
+        let credits = NSAttributedString(
+            string: header + license,
+            attributes: [
+                .font: NSFont.systemFont(ofSize: 10),
+                .foregroundColor: NSColor.labelColor,
+                .paragraphStyle: paragraphStyle,
+            ]
+        )
+
+        NSApplication.shared.orderFrontStandardAboutPanel(options: [.credits: credits])
     }
 }
